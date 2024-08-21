2d779e06e3624bed30d7b00102757ec7888738c7
Uday Kumar Banda <128334885+budaykumar@users.noreply.github.com>
2024-08-21 22:49:33 +0530

    Update index.js

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -10,7 +10,7 @@ const UserProfile = props => {
       <div className="user-details-container">
         <h1 className="user-name"> {name} </h1>
         <p className="user-designation"> {role} </p>
-        <button>Show</button>
+        
       </div>
     </li>
   )
